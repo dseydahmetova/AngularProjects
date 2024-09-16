893614bac924e05cf191aee0353dadeb6f57fcaf
Dana <dana.seidakhmetova@peopleshores.com>
2024-09-16 17:29:33 -0400

    added home component and created the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+  intervalSub;
+
   title = 'my-first-angular-app';
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello From onInit')
+    }, 1000);
+  }
+  ngOnDestroy(): void {
+    if(this.intervalSub)
+      clearInterval(this. intervalSub);
+  }
 }
